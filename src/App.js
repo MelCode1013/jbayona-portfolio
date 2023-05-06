f3371b8de54849fb00d564f896ee1382355d5d5f
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-05-06 10:23:45 -0400

    install styled-components and implement on front end

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
+import {Header,Body} from "./styles/Global";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Body>
+        <Header>J O S H U A&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;B A Y O N A</Header>
+      </Body>
     </div>
   );
 }
