68a46cb8650eff18efa4031e8f72ffc172f5977c
Melinna Bayona <96249341+MelCode1013@users.noreply.github.com>
2023-05-06 21:12:52 -0400

    add gallery to app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BodyStyle } from "./styles/Component.styles";
 import Header from "./components/Header";
+import Gallery from "./Gallery";
 
 function App() {
   return (
     <BodyStyle>
       <Header />
+      <Gallery />
     </BodyStyle>
   );
 }
